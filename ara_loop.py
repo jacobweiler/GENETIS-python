6b4f6eeda2471f01134bcc9a184b365dfe224947
Jacob Weiler <zed8drift@gmail.com>
2025-06-11 06:36:18 -0400

    Updated CI + outlined ara_loop.py

---
+++ ara_loop.py
@@ -0,0 +1,21 @@
+# GENETIS Main Loop
+
+# Imports
+
+
+# setup argparse
+
+
+# Setup savestate
+
+
+# Setup rundata directory for a new run OR go into already created one 
+
+
+# Generate DNA for generation 
+
+
+# Build, simulate, export in XFdtd
+
+
+# Translate XF data into AraSim readable data, run AraSim, calculate fitness
